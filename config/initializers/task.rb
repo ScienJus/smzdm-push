c6efd689650d18024a6b35d91f9f3f0303d93328
ScienJus <xie_enlong@foxmail.com>
2015-10-05 11:07:53 +0800

    每次轮训最多为一个邮箱推送一封邮件

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -8,13 +8,16 @@ scheduler = Rufus::Scheduler.new
 #记录上次采集开始时间
 @last_collect_start_time = Time.now.to_i
 
-scheduler.every '180s' do
+scheduler.every '300s' do
   
   #记录这次采集开始时间
   this_collect_start_time = Time.now.to_i
   
   #因为一次采集可能会请求多次，记录每次请求的时间
   loop_request_time_sort = this_collect_start_time
+  
+  #记录推送信息
+  push_users = {}
   
   #循环请求
   while loop_request_time_sort > @last_collect_start_time do
@@ -40,23 +43,20 @@ scheduler.every '180s' do
   
       #解析返回json
       articles = JSON.parse res.body
-
+      
       #遍历json
       articles.each do |article|
         #更新时间戳
         loop_request_time_sort = article['timesort']
         
         #如果时间戳已经在上次采集时间之前，说明此次采集已经完成
-        # break if loop_request_time_sort < @last_collect_start_time
+        break if loop_request_time_sort < @last_collect_start_time
         
         #处理采集到的数据
-        # puts article['article_title']
         content = article['article_title'] + article['article_content']
         
         #得到所有keyword
         keywords = Keyword.all
-                
-        push_users = {}
                 
         keywords.each do |keyword|
           if content.downcase.include? keyword.name.downcase
@@ -64,19 +64,25 @@ scheduler.every '180s' do
             
             keyword.users.where(active: true).each do |user| 
               if push_users.has_key? user.email
-                push_users[user.email].push keyword.name
+                push_users[user.email][:keywords].push keyword.name unless push_users[user.email][:keywords].include? keyword.name
+                push_users[user.email][:articles].push article unless push_users[user.email][:articles].include? article
               else
-                push_users[user.email] = [keyword.name]
+                push_users[user.email] = {}
+                push_users[user.email][:keywords] = [keyword.name]
+                push_users[user.email][:articles] = [article]
               end
             end
           end
-        end
-        
-        push_users.each do |email, keywords|
-          SubscribeMailer.push_email(email, keywords, article).deliver
-        end
+        end        
       end
     end
   end
-  @LAST_COLLECT_TIME = this_collect_start_time
+  
+  #发布推送信息
+  push_users.each do |email, value|
+    SubscribeMailer.push_email(email, value[:keywords], value[:articles]).deliver
+  end
+  
+  #更新上次采集时间
+  @last_collect_start_time = this_collect_start_time
 end
