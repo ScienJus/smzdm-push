c4fe5e24249fb85ec91f03cf1bd6edbeb93b4e51
ScienJus <xie_enlong@foxmail.com>
2015-10-07 11:28:49 +0800

    生产环境的一些配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'smzdm.scienjus.com', port: 80 }
     
 end
