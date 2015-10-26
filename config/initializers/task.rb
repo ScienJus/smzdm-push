0d2d712e9fdb7d3be0bfb85007d401a13fe99145
ScienJus <xie_enlong@foxmail.com>
2015-10-26 22:23:14 +0800

    增加间隔

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -10,7 +10,7 @@ scheduler = Rufus::Scheduler.new
 #记录上次采集开始时间
 @last_collect_start_time = Time.now.to_i
 
-scheduler.every '600s' do
+scheduler.every '1800s' do
     
   Rails.logger.info "开始采集#{Time.now.to_s}!"
     
